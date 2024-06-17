7883a88f090117a42e5c46948dcd10bb16281b46
Gorun <307532500@qq.com>
2024-06-17 10:56:54 +0800

    添加user agent模拟浏览器请求

--- GameList.py
+++ GameList.py
@@ -4,7 +4,12 @@ from bs4 import BeautifulSoup
 # 目标URL
 url = 'https://www.taptap.cn/'
 
-response = requests.get(url)
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 '
+                  'Safari/537.36 Edg/126.0.0.0'
+}
+
+response = requests.get(url, headers=headers)
 response.encoding = 'utf-8'
 
 html_content = response.text
