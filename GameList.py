ee7c54be71f5f273ee435aff8bd7594b065414d7
Gorun <307532500@qq.com>
2024-06-17 11:35:31 +0800

    添加获取前100个游戏元素的功能

--- GameList.py
+++ GameList.py
@@ -16,32 +16,40 @@ class GameItem:
 
 
 # 目标URL
-url = 'https://www.taptap.cn/top/download/'
+url = 'https://www.taptap.cn/top/download'
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 '
                   'Safari/537.36 Edg/126.0.0.0'
 }
 
-response = requests.get(url, headers=headers)
-response.encoding = 'utf-8'
-
-html_content = response.text
-
-# 使用BeautifulSoup解析HTML内容
-soup = BeautifulSoup(html_content, 'html.parser')
-print(soup.title)
-
 results = list()
-game_list = soup.find('div', class_='list-content')
-for game in game_list.find_all('div', class_='list-item'):
-    game_center = game.find('div', class_='game-center')
-    game_rank = game.find(class_='rank-index').text
-    game_icon_url = game.find('img', alt=re.compile('.*icon$'))['src']
-    game_rating = game.find(class_='tap-rating__number').text
-    game_name = game_center.find('meta', itemprop='name')['content']
-    result = GameItem(game_rank, game_name, game_rating, game_icon_url)
-    results.append(result)
 
+
+def getDataPage(page):
+    response = requests.get(url + f"?page={page}", headers=headers)
+    response.encoding = 'utf-8'
+
+    html_content = response.text
+
+    # 使用BeautifulSoup解析HTML内容
+    soup = BeautifulSoup(html_content, 'html.parser')
+    print(soup.title)
+
+    game_list = soup.find('div', class_='list-content')
+    for game in game_list.find_all('div', class_='list-item', attrs={'data-page': page}):
+        game_center = game.find('div', class_='game-center')
+        game_rank = game.find(class_='rank-index').text
+        game_icon_url = game.find('img', alt=re.compile('.*icon$'))['src']
+        game_rating = game.find(class_='tap-rating__number').text
+        game_name = game_center.find('meta', itemprop='name')['content']
+        result_game_item = GameItem(game_rank, game_name, game_rating, game_icon_url)
+        results.append(result_game_item)
+
+
+print('开始爬取数据...')
+for i in range(1, 11):
+    getDataPage(i)
 for result in results:
     print(result)
+print('爬取数据结束！')
