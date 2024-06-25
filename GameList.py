2caca1c29aa34acd6464cfbce25b969cbb5832e3
maskira <361299572@qq.com>
2024-06-25 09:49:54 +0800

    爬取游戏数据

--- GameList.py
+++ GameList.py
@@ -1,9 +1,10 @@
 import csv
-import logging
 import re
+import time
 
 import requests
 from bs4 import BeautifulSoup
+from lxml import etree
 
 
 class GameItem:
@@ -19,18 +20,19 @@ class GameItem:
 
 
 # 目标URL
-url = 'https://www.taptap.cn/top/download'
+url = 'https://www.taptap.cn/top'
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 '
                   'Safari/537.36 Edg/126.0.0.0'
 }
 
-rank_results = list()
+rank_results: list[GameItem] = list()
 
 
-def get_rank_list(page):
-    response = requests.get(url + f"?page={page}", headers=headers)
+def get_rank_list(group, page):
+    rank_results.clear()
+    response = requests.get(url + f"{group}?page={page}", headers=headers)
     response.encoding = 'utf-8'
 
     html_content = response.text
@@ -41,24 +43,27 @@ def get_rank_list(page):
 
     game_list = soup.find('div', class_='list-content')
     for game in game_list.find_all('div', class_='list-item', attrs={'data-page': page}):
-        game_center = game.find('div', class_='game-center')
-        game_rank = game.find(class_='rank-index').text
-        game_icon_url = game.find('img', alt=re.compile('.*icon$'))['src']
-        game_rating = game.find(class_='tap-rating__number').text
-        game_name = game_center.find('meta', itemprop='name')['content']
-        game_id = re.search(r'/app/(\d+)', game.find('a', class_='game-cell__icon')['href']).group(1)
-        result_rank_item = GameItem(game_rank, game_name, game_rating, game_icon_url, game_id)
-        rank_results.append(result_rank_item)
+        try:
+            game_center = game.find('div', class_='game-center')
+            game_rank = game.find(class_='rank-index').text
+            game_icon_url = game.find('img', alt=re.compile('.*icon$'))['src']
+            game_rating = game.find(class_='tap-rating__number').text
+            game_name = game_center.find('meta', itemprop='name')['content']
+            game_id = re.search(r'/app/(\d+)', game.find('a', class_='game-cell__icon')['href']).group(1)
+            result_rank_item = GameItem(game_rank, game_name, game_rating, game_icon_url, game_id)
+            rank_results.append(result_rank_item)
+        except:
+            continue
 
 
 class GameInfo:
-    def __init__(self):
+    def __init__(self, gameItem: GameItem):
+        self.icon = gameItem.icon_url
         self.name = ''
-        self.downloads = None
-        self.followers = None
         self.size = None
         self.manufacture = None
         self.description = ''
+        self.devNote = ''
         self.album: list[str] = []
         self.tags: list[str] = []
 
@@ -69,9 +74,9 @@ class GameInfo:
         return ','.join(self.tags)
 
 
-def get_game_detail(game_id) -> GameInfo:
-    game_info = GameInfo()
-    response = requests.get(f'https://www.taptap.cn/app/{game_id}', headers=headers)
+def get_game_detail(gameItem: GameItem) -> GameInfo:
+    game_info = GameInfo(gameItem)
+    response = requests.get(f'https://www.taptap.cn/app/{gameItem.id}', headers=headers)
     response.encoding = 'utf-8'
     html_content = response.text
     soup = BeautifulSoup(html_content, 'html.parser')
@@ -88,12 +93,8 @@ def get_game_detail(game_id) -> GameInfo:
             label_text = label.text.strip()
             value_text = value.text.strip()
 
-            if label_text == '下载':
-                game_info.downloads = value_text
-            elif label_text == '游戏大小':
+            if label_text == '游戏大小':
                 game_info.size = value_text
-            elif '热度' in label_text:
-                game_info.heat = value_text
 
         dev_texts = info.find_all('div', {'class': 'tap-text tap-text__one-line'})
         if len(dev_texts) == 2 and ('开发' in dev_texts[0].text or '厂商' in dev_texts[0].text):
@@ -104,15 +105,36 @@ def get_game_detail(game_id) -> GameInfo:
         if follower_label and follower_text and '关注' in follower_label.text:
             game_info.followers = follower_text.text
 
-    game_info.description = get_game_description(game_id)
+    # 将BeautifulSoup对象转换为lxml对象
+    dom = etree.HTML(str(soup))
+
+    developer_info_divs = dom.xpath(
+        '//*[@id="__nuxt"]/div/main/div/div[1]/div[4]/div/div[4]/div[2]/div[3]/div/span[2]')
+
+    if developer_info_divs:
+        # 提取并打印每个找到的div的文本内容
+        for i, div in enumerate(developer_info_divs, 1):
+            developer_text = div.xpath('string(.)').strip()
+            game_info.devNote = developer_text
+    else:
+        developer_info_divs = dom.xpath(
+            '//*[@id="__nuxt"]/div/main/div/div[1]/div[4]/div/div[3]/div[2]/div[3]/div/span[2]')
+        if developer_info_divs:
+            # 提取并打印每个找到的div的文本内容
+            for i, div in enumerate(developer_info_divs, 1):
+                developer_text = div.xpath('string(.)').strip()
+                game_info.devNote = developer_text
+        else:
+            print(game_info.name, 'has no dev note')
+
+    game_info.description = get_game_description(gameItem.id)
 
     album_sliders = soup.find('div', class_='app-trailer-screenshot-header__wrap')
     for image in album_sliders.find_all('img'):
         try:
             game_info.album.append(image['data-src'])
         except KeyError:
-            print(game_info.name, 'has no image')
-            break
+            continue
 
     tags = soup.find('div', class_='app-intro__tag')
     for tag in tags.find_all('a'):
@@ -121,7 +143,7 @@ def get_game_detail(game_id) -> GameInfo:
         except KeyError:
             print(game_info.name, 'has no tag')
             break
-    print(game_info.name, game_info.tags)
+    # print(game_info.name, game_info.tags)
     return game_info
 
 
@@ -147,23 +169,62 @@ def save_to_csv(info_list, filename='game_details.csv'):
 def save_to_sql(info_list: list[GameInfo], filename='game_details.sql'):
     with open(filename, 'w', encoding='utf-8') as output_file:
         output_file.write(
-            "INSERT INTO game(name, downloads, followers, size, manufacture, description, album, tags) \n")
+            "INSERT INTO game(name, icon, size, manufacture, description,dev_note, album, tags) VALUES\n ")
         for info in info_list:
             output_file.write(
-                f"('{info.name}', '{info.downloads}', '{info.followers}', '{info.size}',"
-                f" '{info.manufacture}', '{info.description}', '{info.comma_album()}', '{info.comma_tags()}'),\n"
+                f"('{info.name}', '{info.icon}', '{info.size}',"
+                f" '{info.manufacture}', '{info.description}', '{info.devNote}', '{info.comma_album()}', '{info.comma_tags()}'),\n"
             )
 
 
 game_info_list = list()
 
+fifty_groups = ['/download/action',
+                '/download/strategy',
+                '/download/idle',
+                '/download/single',
+                '/download/causal',
+                '/download/sandbox_survival',
+                '/download/management',
+                '/download/unriddle',
+                '/download/music',
+                '/download/swordsman',
+                '/download/otome',
+                '/download/roguelike',
+                '/download/new',
+                '/sell']
+
+hundred_groups = ['/download/action',
+                  '/played',
+                  '/download',
+                  '/exclusive']
+
 print('开始爬取数据...')
-for i in range(1, 2):
-    get_rank_list(i)
+for i, group in enumerate(fifty_groups):
+    game_info_list.clear()
+    for j in range(1, 6):
+        get_rank_list(group, j)
+        for result in rank_results:
+            try:
+                game_detail = get_game_detail(result)
+                game_info_list.append(game_detail)
+            except:
+                continue
+    print(group, '爬取完成')
+    time.sleep(5)
+    save_to_sql(game_info_list, f'game_details-fifty-{i}.sql')
 
-for result in rank_results:
-    game_detail = get_game_detail(result.id)
-    game_info_list.append(game_detail)
+for i, group in enumerate(hundred_groups):
+    game_info_list.clear()
+    for j in range(1, 16):
+        get_rank_list(group, j)
+        for result in rank_results:
+            try:
+                game_detail = get_game_detail(result)
+                game_info_list.append(game_detail)
+            except:
+                continue
+    print(group, '爬取完成')
+    save_to_sql(game_info_list, f'game_details-hundred-{i}.sql')
+
 print('爬取数据结束！')
-
-save_to_sql(game_info_list, 'game_details.sql')
